b22b7534f0b85a8eac8ac6a83ad0c882de6f50ae
Fabricio Bedoya <fbedoya@o2web.ca>
2016-06-20 10:08:35 -0400

    [f] fix script composer

--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -2,13 +2,14 @@
 
 namespace O2Web\BaseTranslator\Installer;
 
-use ComposerScriptEvent as Event;
+use Composer\Script\Event;
 
 class Installer
 {
     public static function postInstall(Event $event)
     {
         $composer = $event->getComposer();
+        var_dump($composer);
         echo "Installed";
     }
 }
