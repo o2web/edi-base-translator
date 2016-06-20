f433b5b06c7d7f3827e68ef242a3a5caf25f2d00
Fabricio Bedoya <fbedoya@o2web.ca>
2016-06-20 12:24:52 -0400

    [f] fixing script de creation de folder/files

--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -21,42 +21,31 @@ class Installer
         $vendorPath = $composer->getConfig()->get('vendor-dir');
 
         $projectPath = dirname($vendorPath);
-        echo " Project path : $projectPath \n";
 
         if (!file_exists($projectPath . '/' . static::CONFIG_DIR)) {
             mkdir($projectPath . '/' . static::CONFIG_DIR, 0775);
-            echo "Directory config has been created\n";
         }
-        echo "Step 1\n";
         if (!file_exists($projectPath . '/' . static::SOURCE_DIR)) {
             mkdir($projectPath . '/' . static::SOURCE_DIR, 0775);
-            echo "Directory source has been created\n";
         }
-        echo "Step 2\n";
         if (!file_exists($projectPath . '/' . static::LOG_DIR)) {
             mkdir($projectPath . '/' . static::LOG_DIR, 0775);
-            echo "Directory log has been created\n";
         }
-        echo "Step 3\n";
 
         $source = dirname(__DIR__);
-        echo "Source path : $source \n";
 
-        if (!file_exists($projectPath . '/' . static::AUTOLOAD)) {
-            copy($source . '/' . static::AUTOLOAD , $projectPath . '/' . static::AUTOLOAD);
+        if (!file_exists($projectPath . '/' . static::LIB_DIR . '/' . static::AUTOLOAD)) {
+            copy($source . '/' . static::LIB_DIR . '/' . static::AUTOLOAD , $projectPath . '/' . static::AUTOLOAD);
             echo "Autoload file has been copied\n";
         }
-        echo "Step 4\n";
-        if (!file_exists($projectPath . '/' . static::BOOTSTRAP)) {
-            copy($source . '/' . static::BOOTSTRAP , $projectPath . '/' . static::BOOTSTRAP);
+        if (!file_exists($projectPath . '/' . static::LIB_DIR . '/' . static::BOOTSTRAP)) {
+            copy($source . '/' . static::LIB_DIR . '/' . static::BOOTSTRAP , $projectPath . '/' . static::BOOTSTRAP);
             echo "Bootstrap file has been copied\n";
         }
-        echo "Step 5\n";
-        if (!file_exists($projectPath . '/' . static::CONSOLE)) {
-            copy($source . '/' . static::CONSOLE, $projectPath . '/' . static::CONSOLE);
+        if (!file_exists($projectPath . '/' . static::LIB_DIR . '/' . static::CONSOLE)) {
+            copy($source . '/' . static::LIB_DIR . '/' . static::CONSOLE, $projectPath . '/' . static::CONSOLE);
             echo "Console file has been copied\n";
         }
-
 
         echo "\nInstallation has finished\n";
     }
