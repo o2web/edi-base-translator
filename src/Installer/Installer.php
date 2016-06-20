0f698010cb59975b43da53bc0d79ab3b1a3014bf
Fabricio Bedoya <fbedoya@o2web.ca>
2016-06-20 11:41:07 -0400

    [+] log message to follow modifications

--- src/Installer/Installer.php
+++ src/Installer/Installer.php
@@ -24,24 +24,30 @@ class Installer
 
         if (file_exists($projectPath . '/' . static::CONFIG_DIR)) {
             mkdir($projectPath . '/' . static::CONFIG_DIR, 0775);
+            echo "Directory config has been created\r";
         }
         if (file_exists($vendorPath . '/' . static::SOURCE_DIR)) {
             mkdir($projectPath . '/' . static::SOURCE_DIR, 0775);
+            echo "Directory source has been created\r";
         }
         if (file_exists($vendorPath . '/' . static::LOG_DIR)) {
             mkdir($projectPath . '/' . static::LOG_DIR, 0775);
+            echo "Directory log has been created\r";
         }
 
         $source = dirname(__DIR__);
 
         if (file_exists($source . '/' . static::AUTOLOAD)) {
             copy($source . '/' . static::AUTOLOAD , $projectPath . '/' . static::AUTOLOAD);
+            echo "Autoload file has been copied\r";
         }
         if (file_exists($source . '/' . static::BOOTSTRAP)) {
             copy($source . '/' . static::BOOTSTRAP , $projectPath . '/' . static::BOOTSTRAP);
+            echo "Bootstrap file has been copied\r";
         }
         if (file_exists($source . '/' . static::CONSOLE)) {
             copy($source . '/' . static::CONSOLE , $projectPath . '/' . static::CONSOLE);
+            echo "Console file has been copied\r";
         }
 
 
